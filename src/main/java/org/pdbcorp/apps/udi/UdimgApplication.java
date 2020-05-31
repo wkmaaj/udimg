b24c6e9165c1a50ac141693e8fad3f876a652153
jaradat-pdb <jaradat.pdb@gmail.com>
2020-06-01 00:53:19 +0300

    Initial commit of codebase

---
+++ src/main/java/org/pdbcorp/apps/udi/UdimgApplication.java
@@ -0,0 +1,13 @@
+package org.pdbcorp.apps.udi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdimgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdimgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pdbcorp/apps/udi/UdimgApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pdbcorp.apps.udi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdimgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
